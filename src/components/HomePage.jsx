8dc95a5364c8f9f0006a15b09a48859a59ecff78
Cemal <cemalgnlts@gmail.com>
2021-08-29 09:56:56 +0000

    Added scroll to list, FAB edited.

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -12,7 +12,7 @@ import ListToDo from "./ListToDo";
 import { store } from "../store";
 
 function HomePage() {
-    
+
     const openPrompt = () => {
         f7.dialog.prompt("", "New ToDo", text => {
             store.dispatch("add", { text });
@@ -31,7 +31,19 @@ function HomePage() {
                 </Block>
 
                 <Fab slot="fixed" onClick={openPrompt} position="center-bottom">
-                    <div className="plus"></div>
+                    <svg 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        aria-hidden="true"
+                        data-prefix="fas"
+                        data-icon="plus"
+                        className="plus"
+                        viewBox="0 0 448 512"
+                    >
+                        <path 
+                            fill="currentColor"
+                            d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
+                    </svg>
+                    {/* <div className="plus"></div> */}
                 </Fab>
 
             </div>
