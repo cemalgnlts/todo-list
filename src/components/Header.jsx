308cbcbc7aa9e85e5d5d914221630a3862743bb3
Cemal <cemalgnlts@gmail.com>
2021-09-03 15:37:29 +0000

    The date is now displayed relative to the current time.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 
 function Header() {
+    const date = new Date();
+
     return (
         <div className="display-flex flex-direction-row align-items-center">
-            <h1 className="day-number-text">12</h1>
+            <h1 className="day-number-text">{date.getDate()}</h1>
             
             <div className="padding-horizontal-half margin-vertical-auto">
-                <p className="month-text">JAN</p>
-                <p className="year-text">2016</p>
+                <p className="month-text">{date.toLocaleDateString(navigator.language, { month: "short" })}</p>
+                <p className="year-text">{date.getFullYear()}</p>
             </div>
 
-            <p className="day-text">Tuesday</p>
-
+            <p className="day-text">{date.toLocaleDateString(navigator.language, { weekday: "long" })}</p>
         </div>
     );
 }
